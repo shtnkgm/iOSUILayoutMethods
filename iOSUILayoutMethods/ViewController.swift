3e074f7c54f2c1b1f7768d8ef3e9891a4a3d3626
shtnkgm <shtnkgm@gmail.com>
2019-01-30 22:29:10 +0900

    Initial Commit

---
+++ iOSUILayoutMethods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSUILayoutMethods
+//
+//  Created by Shota Nakagami on 2019/01/30.
+//  Copyright © 2019 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
